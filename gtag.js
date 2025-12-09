2ed5808bb42004181a96cf68fe72530a522ee591
MR.X Security <officialcyberkali@gmail.com>
2025-12-09 20:30:45 +0530

    Create gtag.js

---
+++ gtag.js
@@ -0,0 +1,9 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-KN9KRQ5HZL"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-KN9KRQ5HZL');
+</script>
